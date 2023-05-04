effdd8a1851883eecd31bf3021205e7672756f08
Leia Ang <22011199@myrp.edu.sg>
2023-05-04 11:25:51 +0800

    Completed up to section C

--- app/src/main/java/sg/edu/rp/c346/id22011199/week03/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id22011199/week03/MainActivity.java
@@ -3,6 +3,7 @@ package sg.edu.rp.c346.id22011199.week03;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -22,5 +23,16 @@ public class MainActivity extends AppCompatActivity {
         tvDisplay = findViewById(R.id.textViewDisplay);
         buttonDisplay = findViewById(R.id.buttonDisplay);
         etInput = findViewById(R.id.EditTextInput);
+
+        buttonDisplay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Code for the action
+                String stringResponse = etInput.getText().toString();
+                tvDisplay.setText(stringResponse);
+
+            }
+        });
+
     }
 }
